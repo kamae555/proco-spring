cd4e4d9ff82327cfdfe65e0dad5d1f11d0afaa4b
Hyewon Choi <kamae01@gmail.com>
2024-03-03 19:27:35 +0900

    trial commit

---
+++ src/main/java/com/example/procospring/user/SearchCritera.java
@@ -0,0 +1,37 @@
+package com.example.procospring.user;
+
+import lombok.Getter;
+
+@Getter
+public class SearchCritera {
+    // Getter and Setter for country
+    private String country;
+    // Getter and Setter for grade
+    private String grade;
+    // Getter and Setter for language
+    private String language;
+    // Getter and Setter for language2
+    private String language2;
+    // Getter and Setter for dormitory
+    private String dormitory;
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public void setLanguage2(String language2) {
+        this.language2 = language2;
+    }
+
+    public void setDormitory(String dormitory) {
+        this.dormitory = dormitory;
+    }
+}
